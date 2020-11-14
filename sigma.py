047d944fc29c2cdeb1e31e5ed28c61ec8933a9b0
Lorenzo Pes <lollopes1995@gmail.com>
2020-11-15 00:16:29 +0100

    fix indent

--- sigma.py
+++ sigma.py
@@ -5,22 +5,23 @@ def sigma(data):
     for x in range(data_small.shape[0]):
          for row in range(data_small.shape[1]):
              for column in range(data_small.shape[2]):
-                  index1=row
-                  index2=row+1
-                  index3=row-1
-                  index4=column
-                  index5=column+1
-                  index6=column-1       
-                  total=0
-                  try:      
+                 index1=row
+                 index2=row+1
+                 index3=row-1
+                 index4=column
+                 index5=column+1
+                 index6=column-1       
+                 total=0
+                 
+                 try:      
                       if index6<0 or index3<0 :
                           total = data_small[x,index1,index5]  + data_small[x,index2,index4]   + data_small[x,index2,index5]
                       else:   
                           total = data_small[x,index1,index5] + data_small[x,index1,index6] + data_small[x,index2,index4] + data_small[x,index3,index4]+ data_small[x,index3,index5] + data_small[x,index2,index6] + data_small[x,index3,index6] + data_small[x,index2,index5]
                            
-                  except IndexError: 
+                 except IndexError: 
                       total = total +0
         
-                  epsilon[x,row,column] = math.sqrt(6/7) * ( data_small[x,row,column] - ( (1/6)*total ) )
+                 epsilon[x,row,column] = math.sqrt(6/7) * ( data_small[x,row,column] - ( (1/6)*total ) )
     
-  return( 1/(data_small.size) ) * math.pow( np.sum(epsilon),2)
+    return ( 1/(data_small.size) ) * math.pow(np.sum(epsilon),2) 
