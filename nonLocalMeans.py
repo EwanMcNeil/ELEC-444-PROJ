44ce8a79550fed273a37d2ee6a15d2cc1c8faf80
EwanMcNeil <ewanmcneil11@gmail.com>
2020-11-14 10:19:16 -0500

    added guassian noise and random 3d image gen

--- nonLocalMeans.py
+++ nonLocalMeans.py
@@ -4,6 +4,9 @@ import nibabel as nib
 import math
 from scipy.spatial import distance
 
+from random import seed
+from random import randint
+
 #The classical definition of the NL-means filter considers that each voxel can be linked to all the others, but#
 # for practical computational reasons the number of voxels taken into account in the weighted
 #average can be limited to the so-called “search volume” Vi of size (2M+1)3, centered at the current voxel xi.
@@ -12,7 +15,7 @@ from scipy.spatial import distance
 #making the weight function
 
 def weight(tupleI, tupleJ):
-    Z = 100
+    Z = 216
     h = 10
     #h should be 10 times the standard deviation
     #no idea what Z should be 
@@ -35,20 +38,46 @@ def getNewValue(inputTuple, data):
     #with a certain thing but we dont need to go into that rn
 
     sum = 0;
-    for x in range(inputTuple[0]-5, inputTuple[0]+5):
-        for y in range(inputTuple[1]-5, inputTuple[1]+5):
-                 # for z in range(inputTuple[0]-10, inputTuple[0]-10):
-            try:
-                w = weight(inputTuple,(x,y))
-                #,z))
-               
-                sum = sum + w*data[x,y] 
-                #,z]
-            except IndexError:
-                break
+    for x in range(inputTuple[0]-3, inputTuple[0]+3):
+        for y in range(inputTuple[1]-3, inputTuple[1]+3):
+                for z in range(inputTuple[2]-3, inputTuple[2]-3):
+                    try:
+                        w = weight(inputTuple,(x,y))
+                        #,z))
+                    
+                        sum = sum + w*data[x,y] 
+                        #,z]
+                    except IndexError:
+                        break
 
     
     return sum
+
+
+def createImage(size):
+    seed(1)
+    output = np.zeros(shape=(size,size,size));
+    for x in range(size):
+        for y in range(size):
+            for z in range(size):
+                output[x,y,z] = randint(0, 10)
+
+    return output
+
+
+
+##code for guassian noise
+##found online make sure to change later**
+def noisy(image):
+      row,col,ch= image.shape
+      mean = 0
+      var = 0.1
+      sigma = var**0.5
+      gauss = np.random.normal(mean,sigma,(row,col,ch))
+      gauss = gauss.reshape(row,col,ch)
+      noisy = image + gauss
+      return noisy
+
 
 img = nib.load('NormalBrains/t1_icbm_normal_1mm_pn3_rf20.mnc')
 data = img.get_fdata()
@@ -59,20 +88,38 @@ print(data.mean())
 
 
 
-output = np.zeros(shape=(181,217));
+output = np.zeros(shape=(6,6,6));
 #going through each value and caluclulating their new thing
 
-oneSlice = data[:,:,50]
 
-for x in range(181):
-  for y in range(217):
-   # for z in range(181):
-        output[x,y] = getNewValue((x,y),oneSlice)
+groundImage = createImage(6)
+
+noisyImage = noisy(groundImage)
+
+
+for x in range(6):
+ for y in range(6):
+    for z in range(6):
+        output[x,y,z] = getNewValue((x,y,z),noisyImage)
         print(x)
       
 
-plt.imshow(data[:,:,50], interpolation = 'nearest')
+
+
+
+plt.imshow(groundImage[:,:,3], interpolation = 'nearest')
+#axs[0].set_title('inputData')
 plt.show()
 
-plt.imshow(output[:,:], interpolation = 'nearest')
+
+
+
+plt.imshow(noisyImage[:,:,3], interpolation = 'nearest')
+#axs[0].set_title('noisy')
 plt.show()
+
+
+plt.imshow(output[:,:,3], interpolation = 'nearest')
+#axs[0].set_title('noiser')
+plt.show()
+
