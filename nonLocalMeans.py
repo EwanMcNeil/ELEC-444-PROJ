8bd065531b1f3ecf1f2deb19bf3a17dfc1627d42
EwanMcNeil <ewanmcneil11@gmail.com>
2020-11-16 23:11:36 -0500

    dup

--- nonLocalMeans.py
+++ nonLocalMeans.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 import nibabel as nib 
 import math
 from scipy.spatial import distance
+import concurrent.futures
 
 import imageio
 
+from sigma import sigma
 
   
 
@@ -20,29 +22,33 @@ from random import randint
 #making the weight function
 
 
+##they use the connected neighborhood not the 26 one
 ##needs to be done with the neighboorhood intensities 
-def neighboorhoodIntensities(Tuple, data):
+
+
+
+def neighboorhoodIntensities(tuple, data):
     
     
     #M defines the half length of neighboorhood
     output = np.array([])
-    count = 0
-    for x in range(Tuple[0]-1, Tuple[0]+1):
-        for y in range(Tuple[1]-1, Tuple[1]+1):
-            for z in range(Tuple[2]-1, Tuple[2]+1):
-                try:
-                    
-                    output = np.append(output, [data[x,y,z]])
-                    count = count +1
-                except IndexError:
-                    output[1,count] = 0
-                    count = count +1
-                    continue
-        
-  
-   
-    return output
+    vox1 = (tuple[0]-1,tuple[1],tuple[2])
+    vox2 = (tuple[0]+1,tuple[1],tuple[2])
+    vox3 = (tuple[0],tuple[1],tuple[2]+1)
+    vox4 = (tuple[0],tuple[1],tuple[2]-1)
+    vox5 = (tuple[0],tuple[1]+1,tuple[2])
+    vox6 = (tuple[0],tuple[1]-1,tuple[2])
+    iteration = (vox1,vox2,vox3,vox4,vox5,vox6)
 
+    for vox in iteration:
+        try:
+            output = np.append(output, [data[vox]])
+        except IndexError:
+            output = np.append(output, 0)
+            continue
+    
+
+    return output
 
 
 
@@ -50,17 +56,37 @@ def neighboorhoodIntensities(Tuple, data):
 
 def weight(tupleI, tupleJ,data):
     Z = 216
-    h = 0.5
-    #h should be 10 times the standard deviation
-    #no idea what Z should be 
-    uNJ = neighboorhoodIntensities(tupleJ, data)
-    uNI = neighboorhoodIntensities(tupleI, data)
+    global h
 
-    dist = np.linalg.norm(uNJ-uNI)
-    div = dist/(h*h)
-    exponential = math.exp((-1)*div)
 
-    output = (1/Z)*exponential
+    #doing part two with the voxel selection 
+    #dont know the varience so im just doing the mean
+    
+    #uNJ = neighboorhoodIntensities(tupleJ, data)
+    #uNI = neighboorhoodIntensities(tupleI, data)
+
+    #meanJ = np.sum(uNJ)/len(uNJ)
+    #meanI = np.sum(uNI)/len(uNJ)
+
+    #meanDiv = meanJ/meanI
+    global blockMeansVal
+    uNJ = blockMeansVal[tupleJ]
+    uNI = blockMeansVal[tupleI]
+
+    meanDiv = uNI/uNI
+    print(uNJ,uNI,meanDiv)
+
+    output = 0
+
+    if(meanDiv < 1):
+        dist = np.linalg.norm(uNJ-uNI)
+        div = dist/(h*h)
+        
+        #div = dist/(2*1*h*26)
+        exponential = math.exp((-1)*div)
+    
+        output = (1/Z)*exponential
+   
     return output
 
 
@@ -70,34 +96,22 @@ def getNewValue(inputTuple, data):
 
     #need to center around value 
     #with a certain thing but we dont need to go into that rn
-    sum = 0;
     global M 
+    sum = 0
     for x in range(inputTuple[0]-M, inputTuple[0]+M):
         for y in range(inputTuple[1]-M, inputTuple[1]+M):
              for z in range(inputTuple[2]-M, inputTuple[2]+M):
                  try:
-                     if(inputTuple[0]-M < -2 or inputTuple[1]-M < -2 or inputTuple[2]-M < -2):
-                        sum = sum + 0
-                     elif(inputTuple[0]+M > 21 or inputTuple[1]+M > 21 or inputTuple[2]+M > 21):
-                        sum = sum + 0
-                     else:
-                        w = weight(inputTuple,(x,y,z),data)
-                        sum = sum + w*data[x,y,z] 
+                     w = weight(inputTuple,(x,y,z),data)
+                     sum = sum + w*data[x,y,z] 
                  except IndexError:
                      sum = sum + 0
                      continue
+    
+    #print("sum",sum )
     return sum
 
 
-def createImage(size):
-    seed(1)
-    output = np.zeros(shape=(size,size,size));
-    for x in range(size):
-        for y in range(size):
-            for z in range(size):
-                output[x,y,z] = randint(0, 10)
-
-    return output
 
 
 
@@ -119,60 +133,110 @@ data = img.get_fdata()
 print(data.mean())
 
 
-#A voxel represents a single sample, or data point, on a regularly spaced, three-dimensional grid.
+
+outputImage = np.zeros(shape=(75,75,75))
+#going through each value and caluclulating their new thing
+
+noisyImage = noisy(data)
 
 
 ##GLobal M to define the neighborhood
-M = 3
-
-
-output = np.zeros(shape=(20,20,20));
-#going through each value and caluclulating their new thing
-
-im = imageio.imread('heart.png')
-print(im.shape)
-
-
-imageIN = im[:,:,2]
-
-img3D = np.zeros(shape=(20,20,20))
-
-
-for x in range(20):
-  for y in range(20):
-     for z in range(20):
-        img3D[x,y,z] = imageIN[x,y]
-      
-groundImage = createImage(6)
-
-noisyImage = noisy(img3D)
-
-
-for x in range(20):
-  for y in range(20):
-     for z in range(20):
-        output[x,y,z] = getNewValue((x,y,z),noisyImage)
-        print(x)
-
-      
+M = 2
+h = 10
 
 
 
-plt.subplot(1,3,1)
-plt.imshow(img3D[:,:,3], interpolation = 'nearest')
+##precalculating the neighboorhood values 
+
+            
+blockMeansVal = np.zeros(shape=(75,75,75))
+blockVectors = np.zeros(shape=(75,75,75))
+    
+
+##I think you need to make the blocks first and then knowing the increment you find the 
+## array and get the value 
+def blockValues(outputImage, noisyImage):
+   
+    count = 0
+    for x in range(0,75):
+        for y in range(0,75):
+            for z in range(0,75):
+                #need to get mean of its self
+                sum = np.sum(noisyImage[x:x+5,y:y+5,z:z+5])
+                mean = sum/125
+                global blockMeansVal
+                blockMeansVal[x,y,z] = mean
+                
+                count = count + 1
+    
+   
+
+
+##now using the blockMeans array we do nlm for it
+
+def blockNLM(outputImage, noisyImage):
+   
+    count = 0
+    for x in range(0,75):
+        for y in range(0,75):
+            for z in range(0,75):
+                global blockVectors
+                global blockMeansVal
+                blockVectors[x,y,z] = getNewValue((x,y,z),blockMeansVal)
+              
+
+
+
+
+##now after the block means have been calcaled to find the voxel value we average all the block vectors 
+##in its area
+
+
+""" def loopOperation(outputImage):
+   
+     for x in range(0,15):
+        for y in range(0,15):
+            for z in range(0,15):
+                for()
+
+                global blockVectors
+                global blockMeansVal
+                blockVectors[x,y,z] = getNewValue((x,y,z),blockMeansVal) """
+               
+
+
+
+""" 
+items = [((50,52,),outputImage),((53,54),outputImage),((55,56),outputImage),((57,58),outputImage),((59,60),outputImage),((61,62),outputImage),((63,64),outputImage),((65,66),outputImage),((67,68),outputImage),((69,70),outputImage),((71,72),outputImage),((73,75),outputImage)]
+executor = concurrent.futures.ProcessPoolExecutor(12)
+futures = [executor.submit(loopOperation, item) for item in items]
+concurrent.futures.wait(futures) """
+
+
+#
+#loopOperation((0,75),outputImage, noisyImage)
+
+blockValues(outputImage,noisyImage)
+
+blockNLM(outputImage,noisyImage)
+
+
+
+
+""" plt.subplot(1,3,1)
+plt.imshow(data[0:75,0:75,60], interpolation = 'nearest')
 plt.title("ground")
-print(img3D.shape)
-print(noisyImage.shape)
-print(output.shape)
+
 
 
 plt.subplot(1,3,2)
-plt.imshow(noisyImage[:,:,3], interpolation = 'nearest')
+plt.imshow(noisyImage[0:75,0:75,60], interpolation = 'nearest')
 plt.title("noisy")
 
 
+
 plt.subplot(1,3,3)
-plt.imshow(output[:,:,3], interpolation = 'nearest')
+plt.imshow(outputImage[:,:,10], interpolation = 'nearest')
 plt.title("filtered")
 plt.show()
-
+ """
---
+++ sigma.py
@@ -0,0 +1,33 @@
+import numpy as np
+import math
+
+def sigma(data):
+    data_small = data
+    epsilon = np.zeros(data_small.shape)
+    
+    for x in range(data_small.shape[0]):
+         for row in range(data_small.shape[1]):
+             for column in range(data_small.shape[2]):
+                 index1=row
+                 index2=row+1
+                 index3=row-1
+                 index4=column
+                 index5=column+1
+                 index6=column-1       
+                 total=0
+                 
+                 try:      
+                      if index6<0 or index3<0 :
+                          total = data_small[x,index1,index5]  + data_small[x,index2,index4]   + data_small[x,index2,index5]
+                      else:   
+                          total = data_small[x,index1,index5] + data_small[x,index1,index6] + data_small[x,index2,index4] + data_small[x,index3,index4]+ data_small[x,index3,index5] + data_small[x,index2,index6] + data_small[x,index3,index6] + data_small[x,index2,index5]
+                           
+                 except IndexError: 
+                      total = total +0
+                      continue
+        
+                 epsilon[x,row,column] = math.pow( math.sqrt(6/7) * ( data_small[x,row,column] - ((1/6)*total)),2 )
+
+    print(data_small.size)
+    output = (np.sum(epsilon)/(epsilon.size)) 
+    return output
